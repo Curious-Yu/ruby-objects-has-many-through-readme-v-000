96079a09ece41df4e3b3893b4c44cea698d58abb
Wen Yu <wyu10amy@hotmail.com>
2021-06-08 21:45:15 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
 
+  attr_accessor :name, :years
+
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all = []
+  end
+
+  def all
+    @@all << self
+  end
+
+
+
 end
