4244f9c644a52dd9301de15dd0396fa6ee22747e
Wen Yu <wyu10amy@hotmail.com>
2021-06-08 21:44:03 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,16 @@
 class Customer
 
+  attr_accessor :name, :age
 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all = []
+  end
 
-  
+  def all
+    @@all << self
+  end
+
 
 end
