a4372be9a4c7c4e44d729d1fb9d39e667cc5a16b
Wen Yu <wyu10amy@hotmail.com>
2021-06-08 21:47:14 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,5 +1,17 @@
 class Meal
 
-  
+  attr_accessor :waiter, :customer, :total, :tip
+
+  def initialize(waiter, customer)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all = []
+  end
+
+  def all
+    @@all << self
+  end
 
 end
